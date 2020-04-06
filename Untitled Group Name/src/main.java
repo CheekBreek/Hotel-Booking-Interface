2e63944d56617589adb3fa5665b99281c606c491
CheekBreek <62861074+CheekBreek@users.noreply.github.com>
2020-04-05 21:10:13 -0600

    New working main

--- Untitled Group Name/src/main.java
+++ Untitled Group Name/src/main.java
@@ -5,12 +5,6 @@ public class main
 {
     public static void main(String args[])
     {
-    	try {
-        Hotel hotel = new Hotel();
-    	} catch (Exception input) {
-    		System.out.println(input);
-        }
-        
         SwingUtilities.invokeLater(new Runnable()
         {
             @Override
